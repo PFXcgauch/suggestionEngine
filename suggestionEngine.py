078877b6e3d1603db171dd840b36af61993f7d86
PFXcgauch <44211473+PFXcgauch@users.noreply.github.com>
2019-03-27 16:11:53 -0400

    Update suggestionEngine.py

--- suggestionEngine.py
+++ suggestionEngine.py
@@ -5,7 +5,7 @@ Created on Fri Mar 22 15:19:33 2019
 
 @author: Chris Gauch
 @title: Item to Item suggestion engine
-@last update: 03-26-2019
+@last update: 03-27-2019
 @python version: 3.7.2
 @version: Alpha 1.0.0.0
 ===============================================================================
